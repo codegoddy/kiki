310c91143500661a099c92e9a9dabb8ed5210184
Godwin Mayodi <codegoddy@gmail.com>
2025-11-28 19:51:53 +0300

    API-443-001: ensured keys are stored in the .env

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
 from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Settings(BaseSettings):
     database_hostname: str = "localhost"
